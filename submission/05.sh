1c6f16c7e9d7c95abd90bd41a46b733176dac09b
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-04-25 13:14:38 +0300

    Calculate fee

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,18 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+
+txid=b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+tx=$(bitcoin-cli -signet getrawtransaction $txid true)
+
+outputs=$(echo $tx | jq '[.vout[].value] | add')
+
+inputs=0
+for txin in $(echo $tx | jq -r '.vin[].txid'); do
+  vout=$(echo $tx | jq -r ".vin[] | select(.txid == \"$txin\") | .vout")
+  prevtx=$(bitcoin-cli -signet getrawtransaction $txin true)
+  value=$(echo $prevtx | jq ".vout[$vout].value")
+  inputs=$(echo "$inputs + $value" | bc)
+done
+
+fee_btc=$(echo "$inputs - $outputs" | bc -l)
+fee_sats=$(echo "$fee_btc * 100000000 / 1" | bc)
+echo "$fee_sats"
