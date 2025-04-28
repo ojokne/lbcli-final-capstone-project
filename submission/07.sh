6730450be315605dde468517a6f53d848b13a051
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-04-28 16:23:14 +0300

    Get coinbase transaction

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,7 @@
 # what is the coinbase tx in this block 243,834
+
+# get block hash
+hash=$(bitcoin-cli -signet getblockhash 243834)
+
+tx=$(bitcoin-cli -signet getblock "$hash" | jq -r '.tx[0]')
+echo "$tx"
